1333af7b89ace610168a63e0ae73aec82373b0a1
Alyxia Sother <alyxia@riseup.net>
2025-12-03 15:03:46 +0100

    [index] Refactor the whole thing

--- src/web/assets/ckeditorpasteplain/src/index.ts
+++ src/web/assets/ckeditorpasteplain/src/index.ts
@@ -1,5 +1,43 @@
 import { Plugin } from 'ckeditor5/src/core.js';
-import { plainTextToHtml } from 'ckeditor5/src/clipboard.js';
+import type { ClipboardContentInsertionEvent } from 'ckeditor5/src/clipboard.js';
+import type { ModelElement } from 'ckeditor5';
+
+declare global {
+	interface Window {
+		// makes debugging in prod just that little bit easier :)
+		ALYX_DEBUG_LOGGING?: boolean;
+	}
+}
+
+const HTML_HEADING_REGEX = /htmlH\d+/;
+const ILLEGAL_ATTRIBUTES = [ 'bold', 'italic' ];
+
+const cleanElement = ( node: ModelElement ) => {
+	if ( window.ALYX_DEBUG_LOGGING ) {
+		console.log( 'DEBUG(cleanElement): Cleaning node:\n', node );
+	}
+
+	if ( node.name?.startsWith( 'heading' ) || node.name?.match( HTML_HEADING_REGEX ) ) {
+		// @ts-expect-error This isn't *really* a ModelElement which is why this isn't actually `readonly`
+		node.name = 'paragraph';
+	} else if ( window.ALYX_DEBUG_LOGGING ) {
+		console.warn( 'DEBUG(cleanElement): NODE HAS NO NAME!' );
+	}
+
+	for ( const attribute of ILLEGAL_ATTRIBUTES ) {
+		if ( node.hasAttribute( attribute ) ) {
+			node._removeAttribute( attribute );
+		}
+	}
+
+	if ( 'getChildren' in node ) {
+		for ( const child of node.getChildren() ) {
+			cleanElement( child as unknown as ModelElement );
+		}
+	} else if ( window.ALYX_DEBUG_LOGGING ) {
+		console.warn( 'DEBUG(cleanElement): NODE HAS NO getChildren METHOD!' );
+	}
+};
 
 export class PastePlain extends Plugin {
 	public static get pluginName() {
@@ -8,17 +46,21 @@ export class PastePlain extends Plugin {
 
 	public init(): void {
 		const editor = this.editor;
-		const editingView = editor.editing.view;
 
-		editingView.document.on( 'clipboardInput', ( e, data ) => {
-			if ( editor.isReadOnly ) {
-				return;
-			};
+		editor.plugins.get( 'ClipboardPipeline' ).on<ClipboardContentInsertionEvent>( 'contentInsertion', ( _e, data ) => {
+			if ( window.ALYX_DEBUG_LOGGING ) {
+				console.log( 'DEBUG(contentInsertion): triggered with data:\n', data );
+			}
 
-			const dataTransfer = data.dataTransfer;
-			const content = plainTextToHtml( dataTransfer.getData( 'text/plain' ) );
+			for ( const _child of data.content.getChildren() ) {
+				// TODO: Find what this really is
+				const child = _child as unknown as ModelElement;
+				if ( !child.name ) {
+					return;
+				}
 
-			data.content = this.editor.data.htmlProcessor.toView( content );
+				cleanElement( child );
+			}
 		} );
 	}
 }
